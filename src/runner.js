e901df3560afd4f3d39fbbee15bc6885b68b59b4
ssube <ssube@redbrickhealth.com>
2015-02-24 14:57:30 -0600

    Read stdin for runner, accept ws in grammar.

--- src/runner.js
+++ src/runner.js
@@ -1,4 +1,15 @@
 var parser = require("./overloading.js");
 
-var output = parser.parse("1+(2*3)");
-console.log(output);
+var input = "";
+
+process.stdin.setEncoding("utf8");
+process.stdin.on("readable", () => {
+    let chunk = process.stdin.read();
+    if (chunk !== null) {
+        input += chunk;
+    }
+});
+process.stdin.on("end", () => {
+    var output = parser.parse(input);
+    console.log(output);
+});
